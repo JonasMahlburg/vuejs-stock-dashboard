c1388f1f36f950ea48ba644126c5fc7d3e58573d
Jonas Mahlburg <darrylmanga@yahoo.com>
2024-10-04 12:06:47 +0200

    cleared example-Code, set Background

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>Hello World</h1>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
 <style>
+#body{
+margin: 0;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+background: radial-gradient(71.11% 100% at 50% 0%, #020204 14.6%, #011F35 100%) /* warning: gradient uses a rotation that is not supported by CSS and may not behave as expected */;
+width: 100%;
+height: 100vh;
+color: white;
 }
 </style>
