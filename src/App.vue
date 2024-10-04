6c8c6c501728f1d768586e0974886a72fd52b810
Jonas Mahlburg <darrylmanga@yahoo.com>
2024-10-04 13:41:54 +0200

    create first component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template>
-  <div id="app">
-    <h1>Hello World</h1>
-  </div>
+      <BasCard>
+      <h2>Das ist der Inhalt der Karte</h2>
+      <p>Hier steht mehr Text in der Karte.</p>
+    </BasCard>
 </template>
 
 <script>
+import BasCard from './components/BasCard.vue';
+
+export default {
+  name: 'App',
+  components: {
+    BasCard
+  }
+}
 
 </script>
 
@@ -17,5 +26,7 @@ background: radial-gradient(71.11% 100% at 50% 0%, #020204 14.6%, #011F35 100%) 
 width: 100%;
 height: 100vh;
 color: white;
+padding: 300px;
+box-sizing: border-box;
 }
 </style>
---
+++ src/components/BasCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="bas-card">
+    <!-- Hier kannst du den Inhalt der Karte einfügen -->
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BasCard',
+}
+</script>
+
+<style scoped>
+.bas-card {
+  border-radius: 16px;
+  padding: 20px 32px;
+  background: #011F35;
+  margin-top: 20px;
+}
+</style>
